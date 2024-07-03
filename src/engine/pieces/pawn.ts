f3ca7ccfcd3ab1a583fdd67c7ed6c43d7755e349
mircea-craciun-softwire <Mircea.Craciun@softwire.com>
2024-07-03 15:33:29 +0300

    Pawn attacks

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,8 +1,9 @@
 import Piece from './piece';
 import Player from '../player';
+import player from '../player';
 import Board from '../board';
 import Square from "../square";
-import player from "../player";
+import King from "./king";
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -12,33 +13,50 @@ export default class Pawn extends Piece {
     public getAvailableMoves(board: Board) {
         let moves: Square[] = new Array(0);
 
-        let currentSquare: Square = board.findPiece(this);
 
         if(this.player === Player.WHITE) {
-            if(currentSquare.row == 7)return moves;
-
-            if(board.getPiece(Square.at(currentSquare.row + 1, currentSquare.col)) === undefined) {
-                moves.push(Square.at(currentSquare.row + 1, currentSquare.col));
-                if(currentSquare.row === 1){
-                    if(board.getPiece(Square.at(currentSquare.row + 2, currentSquare.col)) === undefined) {
-                        moves.push(Square.at(currentSquare.row + 2, currentSquare.col));
-                    }
-                }
-            }
-
+            this.checkMove(board, moves, 1);
         }else {
-            if(currentSquare.row == 0)return moves;
-            if(board.getPiece(Square.at(currentSquare.row - 1, currentSquare.col)) === undefined) {
-                moves.push(Square.at(currentSquare.row - 1, currentSquare.col));
-                if(currentSquare.row === 6){
-                    if(board.getPiece(Square.at(currentSquare.row - 2, currentSquare.col)) === undefined) {
-                        moves.push(Square.at(currentSquare.row - 2, currentSquare.col));
-                    }
-                }
-            }
-
+            this.checkMove(board, moves, -1);
         }
         return moves;
 
+    }
+
+    checkMove(board: Board,moves: Square[],  direction: number) : void{
+
+        let finalRow: number = this.player === Player.WHITE? 7:0;
+        let currentSquare: Square = board.findPiece(this);
+
+        if(currentSquare.row === finalRow){
+            return;
+        }
+
+        let startingRow: number = this.player === Player.WHITE? 1:6;
+
+        let nextSquare: Square = Square.at(currentSquare.row + direction, currentSquare.col);
+
+        if(board.getPiece(nextSquare) === undefined) {
+            moves.push(Square.at(currentSquare.row + direction, currentSquare.col));
+
+            if(currentSquare.row === startingRow){
+                if(board.getPiece(Square.at(nextSquare.row + direction, nextSquare.col)) === undefined) {
+                    moves.push(Square.at(nextSquare.row + direction, nextSquare.col));
+                }
+            }
+        }
+
+        this.checkAttack(board, moves, direction,1);
+        this.checkAttack(board, moves, direction,-1);
+
+    }
+
+    checkAttack(board: Board, moves: Square[], verticalDir: number, horizontalDir: number):void{
+        let targetSquare: Square = Square.at(board.findPiece(this).row + verticalDir, board.findPiece(this).col + horizontalDir);
+        if(!Board.positionsExists(targetSquare.row, targetSquare.col)) return;
+        let hitPiece: Piece | undefined = board.getPiece(targetSquare);
+        if(hitPiece !== undefined && hitPiece.player !== this.player && !(hitPiece instanceof King)){
+            moves.push(targetSquare);
+        }
     }
 }
