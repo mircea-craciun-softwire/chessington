33a74eca68cbfcaad2ac79bdb9426204ad6dadb0
mircea-craciun-softwire <Mircea.Craciun@softwire.com>
2024-07-03 12:56:02 +0300

    Rooks working hard and hardly working

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,19 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves: Square[] = new Array(0);
+
+        let currentSquare: Square = board.findPiece(this);
+
+        for(let i = 0; i < 8; i++){
+            if(i != currentSquare.row){
+                moves.push(Square.at(i, currentSquare.col));
+            }
+            if(i != currentSquare.col) {
+                moves.push(Square.at(currentSquare.row, i));
+            }
+        }
+
+        return moves;
     }
 }
