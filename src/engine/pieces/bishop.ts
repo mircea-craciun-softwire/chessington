e96f21b268c71da82da83fb5ecb630bdfd288436
mircea-craciun-softwire <Mircea.Craciun@softwire.com>
2024-07-03 13:11:54 +0300

    Bishop go brrrrr

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,26 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves: Square[] = new Array(0);
+
+        let currentSquare: Square = board.findPiece(this);
+
+        for(let i = 0; i < 8;i ++){
+            let difference: number = Math.abs(currentSquare.col - i);
+
+            if(difference === 0)continue;
+
+            let yPositionUp: number = currentSquare.row + difference;
+            let yPositionDown: number = currentSquare.row - difference;
+
+            if(yPositionUp < 8) {
+                moves.push(Square.at(yPositionUp, i))
+            }
+            if(yPositionDown >= 0) {
+                moves.push(Square.at(yPositionDown, i))
+            }
+        }
+
+        return moves;
     }
 }
