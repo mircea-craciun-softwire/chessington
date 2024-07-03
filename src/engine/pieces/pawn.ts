44a8674624f9151878586a1a8d03d903730d634e
mircea-craciun-softwire <Mircea.Craciun@softwire.com>
2024-07-03 12:47:06 +0300

    Pawns move

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
+import player from "../player";
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,22 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves: Square[] = new Array(0);
+
+        let currentSquare: Square = board.findPiece(this);
+
+        if(this.player === Player.WHITE) {
+            moves.push(Square.at(currentSquare.row + 1, currentSquare.col));
+            if(currentSquare.row === 1){
+                moves.push(Square.at(currentSquare.row + 2, currentSquare.col));
+            }
+        }else {
+            moves.push(Square.at(currentSquare.row - 1, currentSquare.col));
+            if(currentSquare.row === 6){
+                moves.push(Square.at(currentSquare.row - 2, currentSquare.col));
+            }
+        }
+        return moves;
+
     }
 }
