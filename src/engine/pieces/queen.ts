064634f829670848ddb9add62d72083bd333c0d2
mircea-craciun-softwire <Mircea.Craciun@softwire.com>
2024-07-03 13:14:36 +0300

    Queen is queening

--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Queen extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,35 @@ export default class Queen extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves: Square[] = new Array(0);
+
+        let currentSquare: Square = board.findPiece(this);
+
+        for(let i = 0; i < 8; i++){
+            if(i != currentSquare.row){
+                moves.push(Square.at(i, currentSquare.col));
+            }
+            if(i != currentSquare.col) {
+                moves.push(Square.at(currentSquare.row, i));
+            }
+        }
+
+        for(let i = 0; i < 8;i ++){
+            let difference: number = Math.abs(currentSquare.col - i);
+
+            if(difference === 0)continue;
+
+            let yPositionUp: number = currentSquare.row + difference;
+            let yPositionDown: number = currentSquare.row - difference;
+
+            if(yPositionUp < 8) {
+                moves.push(Square.at(yPositionUp, i))
+            }
+            if(yPositionDown >= 0) {
+                moves.push(Square.at(yPositionDown, i))
+            }
+        }
+
+        return moves;
     }
 }
